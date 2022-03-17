3850648b18a2757a300ef17098eb951f71cfa7fd
Andrei Mesilov <andreimesilov1993@mail.ru>
2022-03-17 16:14:12 +0200

    Задание 4.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, must_be_immutable, avoid_print, unused_import
 
-import 'dart:developer';
+// Задание 4. Пункт 1. Фаил main.dart является стандартной точкой входа без него риложение не запускается.
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
 class MyApp extends StatelessWidget {
@@ -18,8 +18,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: MySecondWidget(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: MySecondWidget(),
     );
   }
 }
@@ -90,6 +90,10 @@ class _MyHomePageState extends State<MyHomePage> {
 class MyFirstWidget extends StatelessWidget {
   MyFirstWidget({Key? key}) : super(key: key);
   int counter = 0;
+  // Задание 4. Пункт 5. В StatelessWidget нет контекста поэтому будет выходить ошибка.
+  // String testFunction() {
+  //   return context.runtimeType;
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -112,15 +116,41 @@ class MySecondWidget extends StatefulWidget {
 
 class _MySecondWidget extends State<MySecondWidget> {
   // при горячем рестарте сохраняется предыдущее значение счетчика. Потому что у StatefulWidgetа есть состояние.
+
   int counter = 0;
+
+  // Задание 4. Пункт 6. В StatefulWidget есть контекст поэтому функция будет возвращать тип текущего виджета.
+  Type testFunction() {
+    return context.runtimeType;
+  }
+
   @override
   Widget build(BuildContext context) {
     counter++;
     print(counter);
     return Container(
       child: Center(
-        child: Text('Hello $counter'),
+        child: Text('Hello ${testFunction()}'),
       ),
+    );
+  }
+}
+
+// Задание 4. Пункт 3.
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // Задание 4. Пункт 4.
+    return MaterialApp(
+      theme: ThemeData(
+          backgroundColor: Colors.blue,
+          primaryColor: Colors.red,
+          primarySwatch: Colors.pink),
+      // Строка из title отображается как название нашего приложения в списке запущенных приложений.
+      title: 'Задание 4. Пункт 4.',
+      home: MySecondWidget(),
     );
   }
 }
