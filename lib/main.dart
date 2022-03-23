4c42b009727fa935322b1141be40542907865d1f
Andrei Mesilov <andreimesilov1993@mail.ru>
2022-03-23 15:34:43 +0200

    вставил игноры для того чтобы убрать проблемы.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, must_be_immutable, avoid_print, unused_import
-
-// Задание 4. Пункт 1. Фаил main.dart является стандартной точкой входа без него риложение не запускается.
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -19,145 +18,116 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      // home: MySecondWidget(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // ignore: member-ordering-extended
   final String title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final GlobalKey<ScaffoldState> _key = GlobalKey();
+  final _picker = ImagePicker();
+  File? imageFile;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  // ignore: member-ordering-extended
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
+      key: _key,
+      drawer: Container(
+        color: Colors.green,
+        width: MediaQuery.of(context).size.width / 2,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: const [
+            DrawerHeader(
+              child: Text('Drawer header.'),
+              decoration: BoxDecoration(color: Colors.blue),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ButtonStyle(),
-                  onPressed: _decrementCounter,
-                  child: const Icon(Icons.remove),
-                ),
-                FloatingActionButton(
-                  onPressed: _incrementCounter,
-                  tooltip: 'Increment',
-                  child: const Icon(Icons.add),
-                ),
-              ],
+            ListTile(
+              title: Text('Settings...'),
             ),
           ],
         ),
       ),
-    );
-  }
-}
-
-class MyFirstWidget extends StatelessWidget {
-  MyFirstWidget({Key? key}) : super(key: key);
-  // ignore: member-ordering-extended
-  int counter = 0;
-  // Задание 4. Пункт 5. В StatelessWidget нет контекста поэтому будет выходить ошибка.
-  // String testFunction() {
-  //   return context.runtimeType;
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    // при горячем рестарте значение счетчика сбрасывается. Потому что у StatelessWidget нет состояния(каждый раз стоится заново.).
-    counter++;
-    print(counter);
-    // ignore: newline-before-return
-    return Container(
-      child: Center(
-        child: Text('Hello'),
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              final bar = _snackAction;
+              ScaffoldMessenger.of(context).showSnackBar(bar.call());
+            },
+            icon: const Icon(Icons.alarm),
+          ),
+        ],
+      ),
+      body: Center(
+        child: imageFile == null
+            ? const Placeholder()
+            : Image.file(
+                imageFile!,
+              ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.file_present_rounded),
+            tooltip: 'Пример 1 home.',
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera),
+            tooltip: 'Пример 2 message.',
+            label: 'Message',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            if (index == 0) {
+              _pickImageFromGallery();
+            } else {
+              _pickImageFromCamera();
+            }
+          });
+        },
       ),
     );
   }
-}
 
-class MySecondWidget extends StatefulWidget {
-  const MySecondWidget({Key? key}) : super(key: key);
-  @override
-  State<MySecondWidget> createState() => _MySecondWidget();
-}
-
-class _MySecondWidget extends State<MySecondWidget> {
-  // при горячем рестарте сохраняется предыдущее значение счетчика. Потому что у StatefulWidgetа есть состояние.
-
-  int counter = 0;
-
-  // Задание 4. Пункт 6. В StatefulWidget есть контекст поэтому функция будет возвращать тип текущего виджета.
-  Type testFunction() {
-    return context.runtimeType;
-  }
-
-  // ignore: member-ordering-extended
-  @override
-  Widget build(BuildContext context) {
-    counter++;
-    print(counter);
-    // ignore: newline-before-return
-    return Container(
-      child: Center(
-        child: Text('Hello ${testFunction()}'),
+  SnackBar _snackAction() {
+    return SnackBar(
+      content: const Text(
+        'Вы нажали на кнопку аларм в меню appbar.',
+        style: TextStyle(color: Colors.black, fontSize: 20),
+        textAlign: TextAlign.center,
       ),
+      action: SnackBarAction(
+        label: 'Я знаю.',
+        textColor: Colors.white,
+        disabledTextColor: Colors.black,
+        onPressed: () {},
+      ),
+      backgroundColor: Colors.lightBlue,
     );
   }
-}
 
-// Задание 4. Пункт 3.
+  Future<void> _pickImageFromGallery() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() => imageFile = File(pickedFile.path));
+    }
+  }
 
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    // Задание 4. Пункт 4.
-    return MaterialApp(
-      theme: ThemeData(
-        backgroundColor: Colors.blue,
-        primaryColor: Colors.red,
-        primarySwatch: Colors.pink,
-      ),
-      // Строка из title отображается как название нашего приложения в списке запущенных приложений.
-      title: 'Задание 4. Пункт 4.',
-      home: MyFirstWidget(),
-    );
+  Future<void> _pickImageFromCamera() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+    if (pickedFile != null) {
+      setState(() => imageFile = File(pickedFile.path));
+    }
   }
 }
